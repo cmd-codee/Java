af6fd506774024568bb35400c3b4ea07b60eedba
cmd <161607150+cmd-codee@users.noreply.github.com>
2024-03-02 17:44:10 +0300

    Add files via upload

---
+++ imt.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class imt {
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите свой рост:");
+        double height = scan.nextInt();
+        System.out.println("Введите свой вес:");
+        double weight = scan.nextInt();
+        double m = height/100;
+	    double result = weight/(m*m);
+        if (result < 18.5)
+        {
+            System.out.println("У вас недостаточный вес");
+        }
+        if (result > 18.5 && result < 24.9)
+        {
+            System.out.println("У вас нормальный вес");
+        }
+        if (result > 24.9 && result < 29.9)
+        {
+            System.out.println("У вас избуточный вес");
+        }
+        if (result > 29.9)
+        {
+            System.out.println("У вас ожирение");
+        }
+    }
+}
